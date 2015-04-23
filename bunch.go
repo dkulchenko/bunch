ab7f677123744cba2cf6f6bfdc04140fb4609d82
Daniil Kulchenko <daniil@kulchenko.com>
2015-04-23 14:39:13 -0700

    more scaffolding

--- bunch.go
+++ bunch.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "bunch"
+	app.Usage = "npm-like tool for managing Go dependencies"
+	app.Version = "0.0.1"
+	app.Authors = []cli.Author{cli.Author{Name: "Daniil Kulchenko", Email: "daniil@kulchenko.com"}}
+
+	app.Run(os.Args)
 }
