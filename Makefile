77d0e431d3d9c77d62364b4a5b9fe2a868b573a7
Daniil Kulchenko <daniil@kulchenko.com>
2015-04-23 19:16:09 -0700

    add initial Bunchfile, start on command layout, Makefile cleanup

--- Makefile
+++ Makefile
@@ -6,17 +6,17 @@ bootstrap:
 	@gox -build-toolchain
 
 setup:
-	@mkdir build || true
+	@mkdir build bin || true
 	go get -u github.com/mitchellh/gox
 
 build:
-	@go build .
+	@go build -o bin/bunch . 
 
 run:
-	@./bunch
+	@bin/bunch
 
 clean:
-	@rm bunch >/dev/null 2>&1 || true
+	@rm bin/bunch >/dev/null 2>&1 || true
 
 fullclean: clean
 	@rm -fr build/*
@@ -25,25 +25,21 @@ create-zip:
 	@mkdir -p build/bunch
 	@mv bunch_$(build_os)$(dest_ext) build/bunch/bunch$(dest_ext)
 	@cp README.md build/bunch/README
-	@cp conf/example.yml build/bunch/example-config.yml
 	@cd build && zip -r bunch_$(build_os).zip bunch
 	@rm -r build/bunch
 
 build-linux: clean
-	@go-bindata -prefix sqlite-bin/linux/ sqlite-bin/linux/
 	@gox -osarch="linux/386"
 	@gox -osarch="linux/amd64"
 	@$(MAKE) create-zip build_os=linux_386
 	@$(MAKE) create-zip build_os=linux_amd64
 
 build-osx: clean
-	@go-bindata -prefix sqlite-bin/osx/ sqlite-bin/osx/
 	@gox -os="darwin"
 	@$(MAKE) create-zip build_os=darwin_386
 	@$(MAKE) create-zip build_os=darwin_amd64
 
 build-windows: clean
-	@go-bindata -prefix sqlite-bin/windows/ sqlite-bin/windows/
 	@gox -os="windows"
 	@$(MAKE) create-zip build_os=windows_386 dest_ext=.exe
 	@$(MAKE) create-zip build_os=windows_amd64 dest_ext=.exe
--- bunch.go
+++ bunch.go
@@ -13,5 +13,163 @@ func main() {
 	app.Version = "0.0.1"
 	app.Authors = []cli.Author{cli.Author{Name: "Daniil Kulchenko", Email: "daniil@kulchenko.com"}}
 
+	app.Commands = []cli.Command{
+		{
+			Name:    "install",
+			Aliases: []string{"i"},
+			Usage:   "install package(s)",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "save",
+					Usage: "save installed package to Bunchfile",
+				},
+				cli.BoolFlag{
+					Name:  "g",
+					Usage: "install package to global $GOPATH instead of vendored directory",
+				},
+			},
+			Action: func(c *cli.Context) {
+				installCommand(c)
+			},
+		},
+		{
+			Name:    "update",
+			Aliases: []string{"u"},
+			Usage:   "update package(s)",
+			Action: func(c *cli.Context) {
+				updateCommand(c)
+			},
+		},
+		{
+			Name:    "uninstall",
+			Aliases: []string{"r"},
+			Usage:   "uninstall package(s)",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "save",
+					Usage: "save uninstalled package to Bunchfile",
+				},
+				cli.BoolFlag{
+					Name:  "g",
+					Usage: "uninstall package from global $GOPATH instead of vendored directory",
+				},
+			},
+			Action: func(c *cli.Context) {
+				uninstallCommand(c)
+			},
+		},
+		{
+			Name:  "prune",
+			Usage: "remove packages not referenced in Bunchfile",
+			Action: func(c *cli.Context) {
+				pruneCommand(c)
+			},
+		},
+		{
+			Name:  "outdated",
+			Usage: "list outdated packages",
+			Action: func(c *cli.Context) {
+				outdatedCommand(c)
+			},
+		},
+		{
+			Name:  "lock",
+			Usage: "generate a file locking down current versions of dependencies",
+			Action: func(c *cli.Context) {
+				lockCommand(c)
+			},
+		},
+		{
+			Name:  "rebuild",
+			Usage: "rebuild all dependencies",
+			Action: func(c *cli.Context) {
+				rebuildCommand(c)
+			},
+		},
+		{
+			Name:  "generate",
+			Usage: "generate a Bunchfile based on package imports in current directory",
+			Action: func(c *cli.Context) {
+				generateCommand(c)
+			},
+		},
+		{
+			Name:  "go",
+			Usage: "run a Go command within the vendor environment (e.g. bunch go fmt)",
+			Action: func(c *cli.Context) {
+				goCommand(c)
+			},
+		},
+		{
+			Name:  "exec",
+			Usage: "run any command within the vendor environment (e.g. bunch exec make)",
+			Action: func(c *cli.Context) {
+				execCommand(c)
+			},
+		},
+		{
+			Name:  "shell",
+			Usage: "start a shell within the vendor environment",
+			Action: func(c *cli.Context) {
+				shellCommand(c)
+			},
+		},
+		{
+			Name:  "shim",
+			Usage: "sourced in .bash_profile to alias the 'go' tool",
+			Action: func(c *cli.Context) {
+				shimCommand(c)
+			},
+		},
+	}
+
 	app.Run(os.Args)
+}
+
+func installCommand(c *cli.Context) {
+
+}
+
+func updateCommand(c *cli.Context) {
+
+}
+
+func uninstallCommand(c *cli.Context) {
+
+}
+
+func pruneCommand(c *cli.Context) {
+
+}
+
+func outdatedCommand(c *cli.Context) {
+
+}
+
+func lockCommand(c *cli.Context) {
+
+}
+
+func rebuildCommand(c *cli.Context) {
+
+}
+
+func generateCommand(c *cli.Context) {
+
+}
+
+func goCommand(c *cli.Context) {
+
+}
+
+func execCommand(c *cli.Context) {
+
+}
+
+func shellCommand(c *cli.Context) {
+
+}
+
+func shimCommand(c *cli.Context) {
+
 }
